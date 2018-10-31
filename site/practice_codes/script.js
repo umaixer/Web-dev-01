63abc309f486586de41a7f8caf102e759ac0d438
Umair Aslam <aslam.umair92@gmail.com>
2018-10-31 16:28:55 -0600

    oct 31-4

---
+++ site/practice_codes/script.js
@@ -0,0 +1,21 @@
+/*
+
+var message="in global"
+console.log("global message = " + message)
+var a =function(){
+	var message="inside a"
+console.log(" a:message " + message) 
+b();
+}
+
+function b(){
+	console.log(" b:message " + message)
+}
+a();
+
+
+*/
+
+var x=5;
+x=6;
+console.log(x);
