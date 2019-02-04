a76af419c210cdb16304a857ee68c3a44aae0201
Umair Aslam <aslam.umair92@gmail.com>
2019-02-04 16:30:49 -0600

    Feb 04 mid

---
+++ site/practice_codes/site/ajax_intro/scripts.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded",function (event) {
+	
+	document.querySelector("button").addEventListener("click", function () {
+	
+
+		$ajaxUtils.sendGetRequest("/data/name.txt", function (request) {
+			var name = request.responseText;
+			console.log(self.name);
+				document.querySelector("#content").innerHTML= "<h2> Hello " + name + "!";
+		});
+	
+	});	
+}
+
+);
