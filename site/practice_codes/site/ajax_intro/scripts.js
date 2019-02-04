37f6a49932a9b22e21ebd5907b803b6ba4171b0f
Umair Aslam <aslam.umair92@gmail.com>
2019-02-04 16:36:57 -0600

    Feb 04 end

--- site/practice_codes/site/ajax_intro/scripts.js
+++ site/practice_codes/site/ajax_intro/scripts.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded",function (event) {
 	document.querySelector("button").addEventListener("click", function () {
 	
 
-		$ajaxUtils.sendGetRequest("/data/name.txt", function (request) {
+		$ajaxUtils.sendGetRequest("data/name.txt", function (request) {
 			var name = request.responseText;
 			console.log(self.name);
 				document.querySelector("#content").innerHTML= "<h2> Hello " + name + "!";
