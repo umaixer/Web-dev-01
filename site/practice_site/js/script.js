cbc88b0ea32a2d5fd86319d33abc4b7dfa1b791e
Umair Aslam <aslam.umair92@gmail.com>
2019-02-08 14:38:36 -0600

    Feb 08 05

--- site/practice_site/js/script.js
+++ site/practice_site/js/script.js
@@ -12,6 +12,9 @@ $(function () {
 var ub = {};
 
 var homeHTML = "snippets/home-snippet.html";
+var allCategoriesUrl= "http://davids-restaurant.herokuapp.com/categories.json";
+var categoriesTitleHtml= "snippets/categories-title-snippet.html";
+var categoryHtml = "snippets/category-snippet.html";
 
 var insertHTML = function (selector, html) {
 	var targetElem = document.querySelector(selector);
@@ -24,6 +27,12 @@ var showLoading = function (selector) {
 	insertHTML(selector, html);
 };
 
+var inserProperty = function (string, propName, propValue) {
+	var propToReplace = "{{" + propName + "}}";
+	string = string.replace(new RegExp(propToReplace, "g"),propValue);
+	return string;
+};
+
 document.addEventListener("DOMContentLoaded", function (event) {
 	showLoading("#main-content");
 
@@ -32,6 +41,40 @@ document.addEventListener("DOMContentLoaded", function (event) {
 			document.querySelector("#main-content").innerHTML = responseText;
 		}, false);
 });
+
+ub.loadMenuCategories=function () {
+	showLoading("#main-content");
+	$ajaxUtils.sendGetRequest(allCategoriesUrl, buildAndShowCategoriesHTML);
+
+};
+
+function buildAndShowCategoriesHTML(categories) {
+	$ajaxUtils.sendGetRequest(categoriesTitleHtml, function (categoriesTitleHtml) {
+		$ajaxUtils.sendGetRequest(categoryHtml, function (categoryHtml) {
+			var categoriesViewHTML = buildcategoriesViewHTML
+			(categories,categoriesTitleHtml,categoryHtml);
+			insertHTML("main-content",categoriesViewHTML);
+		}, false);
+	}, false);
+}
+
+function buildcategoriesViewHTML(categories,categoriesTitleHtml,categoryHtml) {
+	var finalHTML= categoriesTitleHtml;
+	finalHTML += "<section class='row'>";
+
+	for (var i = 0; i < categories.length; i++) {
+
+		var html = categoryHtml;
+		var name = "" + categories[i].name;
+		var short_name = categories[i].short_name;
+
+		html= insertHTML(html,"name", name);
+		html= insertHTML(html,"short_name", short_name);
+		finalHTML += html;
+	}
+	finalHTML += "</section>";
+	return finalHTML;
+}
 
 global.$ub = ub;
 
